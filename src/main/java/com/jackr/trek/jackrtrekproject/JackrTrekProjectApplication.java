7d67b2107fe0720c2f1f9fa6772f2f2e4f67d263
asanders7 <alicia.sanders88@hotmail.com>
2021-06-06 11:45:53 -0400

    first commit

---
+++ src/main/java/com/jackr/trek/jackrtrekproject/JackrTrekProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jackr.trek.jackrtrekproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackrTrekProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackrTrekProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackr/trek/jackrtrekproject/JackrTrekProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackr.trek.jackrtrekproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackrTrekProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
