2997001529cf79a8595222b79ea1329450068422
asanders7 <alicia.sanders88@hotmail.com>
2021-06-07 20:24:43 -0400

    Second commit

--- src/main/java/Trek.java
+++ src/main/java/Trek.java
@@ -47,12 +47,54 @@ public class Trek {
     public String getTrekPhysicalRequirements() {
         return trekPhysicalRequirements;
     }
-    
 
-    public Trek(String trekType, String trekDescription, String trekDifficultyLevel) {
+    public String getTrekTips() {
+        return trekTips;
+    }
+
+    public String getTrekLandmarks() {
+        return trekLandmarks;
+    }
+
+    public String getTrekCampsiteInfo() {
+        return trekCampsiteInfo;
+    }
+
+    public String getTrekTerrain() {
+        return trekTerrain;
+    }
+
+    public String getTrekTransportation() {
+        return trekTransportation;
+    }
+
+    public String getNearbyActivities() {
+        return nearbyActivities;
+    }
+
+    public String getTrekCost() {
+        return trekCost;
+    }
+
+    public String getTrekLength() {
+        return trekLength;
+    }
+
+    public String getTrekReviews() {
+        return trekReviews;
+    }
+
+    public Trek(String trekType, String trekDescription, String trekDifficultyLevel, boolean trekGuidedOrNot,
+                String trekPhysicalRequirements, String trekTips, String trekLandmarks, String trekCampsiteInfo,
+                String trekTerrain, String trekTransportation, String nearbyActivities, String trekCost,
+                String trekLength, String trekReviews) {
         this.trekType = trekType;
         this.trekDescription = trekDescription;
         this.trekDifficultyLevel = trekDifficultyLevel;
+        this.trekGuidedOrNot = trekGuidedOrNot;
+        this.trekPhysicalRequirements = trekPhysicalRequirements;
+        this.trekTips = trekTips;
+
     }
 
     @Override
