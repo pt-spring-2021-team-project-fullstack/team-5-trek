7b2caacca258d2c85f64febb1161a869cedd1a46
BrentHajek <brenthajek@hotmail.com>
2021-06-10 22:46:26 -0400

    built continent controller and 2 view templates

---
+++ src/main/java/com/jackr/trek/jackrtrekproject/ContinentRepository.java
@@ -0,0 +1,8 @@
+package com.jackr.trek.jackrtrekproject;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContinentRepository extends CrudRepository<Continent, Long> {
+
+    Continent findByContinent(String Continent);
+}
---
+++ src/main/java/com/jackr/trek/jackrtrekproject/TrekTypeRepository.java
@@ -0,0 +1,11 @@
+package com.jackr.trek.jackrtrekproject;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface TrekTypeRepository extends CrudRepository<TrekType, Long> {
+
+    Optional<TrekType> findByTrekTypePerContinent(String trekTypePerContinent);
+    Optional<TrekType> findByTrekTypePerContinentRegion(String trekTypePerContinentRegion);
+}
