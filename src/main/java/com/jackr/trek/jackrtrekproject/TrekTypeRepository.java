d464de57ca9baa4bb40368ee2e0ed0b9610ea184
BrentHajek <70384347+BrentHajek@users.noreply.github.com>
2021-06-18 14:24:17 -0400

    Revert "Images from brent"

--- src/main/java/com/jackr/trek/jackrtrekproject/TrekTypeRepository.java
+++ src/main/java/com/jackr/trek/jackrtrekproject/TrekTypeRepository.java
@@ -6,7 +6,5 @@ public interface TrekTypeRepository extends CrudRepository<TrekType, Long> {
 
     TrekType findTrekTypeByDifficultyLevel(String difficultyLevel);
 
-//    TrekType findById(long id);
-
 //Optional<TrekType> findByTrekTypePerContinent(String trekTypePerContinent);
 }
